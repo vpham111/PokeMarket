a0586719b1af885fba95b6b042f1092dc951b5e8
Justin Nguyen <justinnguyen2026@gmail.com>
2025-06-12 15:51:32 -0700

    Added FrontEnd and BackEnd frameworks

---
+++ pokemarket/src/test/java/com/pokemarket/pokemarket/PokemarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemarket.pokemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
