424b3b3dbd958d972c507d2bd0fab56e0cf91a06
Vincent Pham <vincep3@uci.edu>
2025-06-13 00:26:50 -0700

    switched frontend to NextJS and created some database Schemas

---
+++ backend/schemas.sql
@@ -0,0 +1,46 @@
+
+CREATE TABLE Set (
+    id UUID PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    release_date DATE
+);
+
+CREATE TABLE Card (
+    id UUID PRIMARY KEY,
+    name VARCHAR,
+    setId UUID REFERENCES Set(id),
+    rarity VARCHAR (50),
+    card_number VARCHAR(20)
+);
+
+CREATE TABLE Users (
+    id UUID PRIMARY KEY,
+    firstname VARCHAR NOT NULL,
+    lastname VARCHAR NOT NULL,
+    email VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+    joinedAt DATE,
+    bio TEXT
+);
+
+CREATE TYPE listingStatus AS ENUM ('Active', 'Sold');
+CREATE TABLE Listings (
+    id UUID PRIMARY KEY ,
+    cardId UUID references Card(id),
+    sellerId UUID references Users(id),
+    price DECIMAL NOT NULL ,
+    quantity INT,
+    createdAt timestamp,
+    status listingStatus
+);
+
+CREATE TYPE transactionStatus AS ENUM ('Pending', 'Completed', 'Cancelled');
+CREATE TABLE Transaction (
+    id UUID PRIMARY KEY ,
+    buyerId UUID references Users(id),
+    listingId UUID references Listings(id),
+    salePrice DECIMAL,
+    quantity INT,
+    status transactionStatus,
+    purchaseDate TIMESTAMP
+);
